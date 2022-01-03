7927e4b0d4070582e9802090aaba8a1bb0ae9366
Muizz Soomar <muizzsoomar@gmail.com>
2022-01-03 00:14:16 -0600

    finished product

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
+import Home from './Home';
+import Trending from './Trending';
+import Feedback from './Feedback';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('loads home page', async () => {
+  render(<Home url="/" />)
+});
+
+test('loads trending page', async () => {
+  render(<Trending url="/trending" />)
+});
+
+test('loads feedback page', async () => {
+  render(<Feedback url="/feedback" />)
 });
